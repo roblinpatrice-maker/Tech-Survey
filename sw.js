5e4f424a1260c5a031c2391913f80b6f34d1e0fd
Patrice Roblin App <roblinpatrice@gmail.com>
2026-03-27 12:14:16 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,38 @@
-self.addEventListener('install', function() { self.skipWaiting(); });
-self.addEventListener('activate', function(e) {
-  e.waitUntil(caches.keys().then(function(keys) {
-    return Promise.all(keys.map(function(k) { return caches.delete(k); }));
-  }));
+const CACHE = 'rekon-recce-v1';
+const ASSETS = [
+  '/j3m9k/',
+  '/j3m9k/index.html',
+  '/j3m9k/manifest.json',
+  '/j3m9k/sw.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(response => {
+        if (response && response.status === 200) {
+          const clone = response.clone();
+          caches.open(CACHE).then(cache => cache.put(e.request, clone));
+        }
+        return response;
+      }).catch(() => caches.match('/j3m9k/index.html'));
+    })
+  );
 });
